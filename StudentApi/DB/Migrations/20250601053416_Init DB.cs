14e3d4999673069d9b12e9610dffae646dd4b706
Rahul Dey <rahuldey9623@gmail.com>
2025-06-01 06:07:18 +0000

    Added database migration

---
+++ StudentApi/DB/Migrations/20250601053416_Init DB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentApi.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Roll = table.Column<int>(type: "integer", nullable: false),
+                    Age = table.Column<int>(type: "integer", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    EmailAddress = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: false),
+                    Gender = table.Column<int>(type: "integer", nullable: false),
+                    Education = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Occupation = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Experience = table.Column<int>(type: "integer", nullable: false),
+                    Salary = table.Column<decimal>(type: "numeric", nullable: false),
+                    MaritalStatus = table.Column<int>(type: "integer", nullable: false),
+                    NumberOfChildren = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
